589c879cc17bcfc10c082c484c66a915d3af952b
Sven Baumann <baumann.sv@gmail.com>
2015-06-25 15:04:58 +0200

    add the class CssToInlineStyle

---
+++ src/Avisota/Message/Render/CssToInlineStyle.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ *
+ * Copyright (C) Avisota
+ *
+ * @package   contao-css-to-inline-style
+ * @file      CssToInlineStyle.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @license   LGPL-3.0+
+ * @copyright Copyright 2015 Avisota
+ */
+
+
+namespace Avisota\Message\Render;
+
+use Avisota\Contao\Message\Core\Event\RenderMessageEvent;
+
+class CssToInlineStyle
+{
+    public static function generate(RenderMessageEvent $event)
+    {
+
+    }
+}
