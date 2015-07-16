cadc11a94008d39656a83664ac7378c227bf63a3
Sven Baumann <baumann.sv@gmail.com>
2015-07-16 06:08:04 +0200

    reform code

--- src/Avisota/Message/Renderer/CssToInlineStyle.php
+++ src/Avisota/Message/Renderer/CssToInlineStyle.php
@@ -32,27 +32,27 @@ class CssToInlineStyle
     {
         if ($GLOBALS['AVISOTA']['ENABLE_CSS_TO_INLINE_STYLE']) {
             //TODO add the methoic to set the css rules as inline styles
-            $content = $event->getPreRenderedMessageTemplate()->getContent();
+            $content                 = $event->getPreRenderedMessageTemplate()->getContent();
             $libxmlUseInternalErrors = libxml_use_internal_errors(true);
-            $document = new \DOMDocument('1.0','UTF-8');
-            $document->formatOutput = true;
+            $document                = new \DOMDocument('1.0', 'UTF-8');
+            $document->formatOutput  = true;
             $document->loadHTML($content);
-            $xpath = new \DOMXPath($document);
+            $xpath  = new \DOMXPath($document);
             $styles = $xpath->query('/html/head/style');
             for ($i = 0; $i < $styles->length; $i++) {
-                if($i<> 0) {
-                    $style = $styles->item($i);
+                if ($i <> 0) {
+                    $style       = $styles->item($i);
                     $inlineStyle = $style;
                     $style->parentNode->removeChild($style);
                 }
             }
-            $content = $document->saveHTML();
-            $htmlInStyle = new \TijsVerkoyen\CssToInlineStyles\CssToInlineStyles($content,$inlineStyle->textContent);
-            $content = $htmlInStyle->convert();
+            $content     = $document->saveHTML();
+            $htmlInStyle = new \TijsVerkoyen\CssToInlineStyles\CssToInlineStyles($content, $inlineStyle->textContent);
+            $content     = $htmlInStyle->convert();
 
             $content = str_replace(
                 array('%5B', '%5D', '%7B', '%7D', '%20'),
-                array('[', ']', '{',   '}',   ' '),
+                array('[', ']', '{', '}', ' '),
                 $content
             );
             $content = preg_replace_callback(
