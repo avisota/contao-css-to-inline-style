07300beac7b724532df9c245ef177bba672137be
Sven Baumann <baumann.sv@gmail.com>
2016-02-22 14:14:41 +0100

    reform code

--- src/CssToInlineStyle.php
+++ src/CssToInlineStyle.php
@@ -39,45 +39,45 @@ class CssToInlineStyle
             return;
         }
 
-            $content                 = $event->getPreRenderedMessageTemplate()->getContent();
-            $libxmlUseInternalErrors = libxml_use_internal_errors(true);
-            $document                = new \DOMDocument('1.0', 'UTF-8');
-            $document->formatOutput  = true;
-            $document->loadHTML($content);
-            $xpath  = new \DOMXPath($document);
-            $styles = $xpath->query('/html/head/style');
-            for ($i = 0; $i < $styles->length; $i++) {
-                if ($i <> 0) {
-                    $style       = $styles->item($i);
-                    $inlineStyle = $style;
-                    $style->parentNode->removeChild($style);
-                }
+        $content                 = $event->getPreRenderedMessageTemplate()->getContent();
+        $libxmlUseInternalErrors = libxml_use_internal_errors(true);
+        $document                = new \DOMDocument('1.0', 'UTF-8');
+        $document->formatOutput  = true;
+        $document->loadHTML($content);
+        $xpath  = new \DOMXPath($document);
+        $styles = $xpath->query('/html/head/style');
+        for ($i = 0; $i < $styles->length; $i++) {
+            if ($i <> 0) {
+                $style       = $styles->item($i);
+                $inlineStyle = $style;
+                $style->parentNode->removeChild($style);
             }
-            $content     = $document->saveHTML();
-            $htmlInStyle = new CssToInlineStyles($content, $inlineStyle->textContent);
-            $content     = $htmlInStyle->convert();
+        }
+        $content     = $document->saveHTML();
+        $htmlInStyle = new CssToInlineStyles($content, $inlineStyle->textContent);
+        $content     = $htmlInStyle->convert();
 
-            $content = str_replace(
-                array('%5B', '%5D', '%7B', '%7D', '%20'),
-                array('[', ']', '{', '}', ' '),
-                $content
-            );
-            $content = preg_replace_callback(
-                '~\{%.*%\}~U',
-                function ($matches) {
-                    return html_entity_decode($matches[0], ENT_QUOTES, 'UTF-8');
-                },
-                $content
-            );
-            $content = preg_replace_callback(
-                '~##.*##~U',
-                function ($matches) {
-                    return html_entity_decode($matches[0], ENT_QUOTES, 'UTF-8');
-                },
-                $content
-            );
-            $event->getPreRenderedMessageTemplate()->setContent($content);
-            libxml_use_internal_errors($libxmlUseInternalErrors);
-            return;
+        $content = str_replace(
+            array('%5B', '%5D', '%7B', '%7D', '%20'),
+            array('[', ']', '{', '}', ' '),
+            $content
+        );
+        $content = preg_replace_callback(
+            '~\{%.*%\}~U',
+            function ($matches) {
+                return html_entity_decode($matches[0], ENT_QUOTES, 'UTF-8');
+            },
+            $content
+        );
+        $content = preg_replace_callback(
+            '~##.*##~U',
+            function ($matches) {
+                return html_entity_decode($matches[0], ENT_QUOTES, 'UTF-8');
+            },
+            $content
+        );
+        $event->getPreRenderedMessageTemplate()->setContent($content);
+        libxml_use_internal_errors($libxmlUseInternalErrors);
+        return;
     }
 }
